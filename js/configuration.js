b758b5e9ede61b7eb8b42bd2f945a31f1f4f3f1b
VesperHuang <deepvesper@gmail.com>
2017-11-18 11:55:50 +0800

    Initial commit

---
+++ js/configuration.js
@@ -0,0 +1,30 @@
+﻿var MovieTickets = {};
+
+//try
+//{
+//    xhr = new window.XMLHttpRequest(); //创建XML HttpRequest对象
+//    //_xhr.setRequestHeader("Origin", '*');
+//}
+//catch (ex)
+//{
+//    try
+//    {
+//        xhr = new ActiveXObject('Msxml2.XMLHTTP'); //低版本兼容
+//        //_xhr.setRequestHeader("Origin", '*');
+//    }
+//    catch (ex)
+//    {
+
+//    }
+//};
+
+MovieTickets.configuration = (function () {
+    //var serviceUrl = "http://172.28.0.50:82/api/";
+    var serviceUrl = "http://180.166.253.196:84/api/";
+    
+    return {
+        serviceUrl: serviceUrl,
+        accountUrl: serviceUrl + "Account/",
+        reportUrl: serviceUrl + "Report/PieCharts/"
+    }
+})();
